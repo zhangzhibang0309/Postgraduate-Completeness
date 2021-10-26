d995739df4902775e6efb5a4162eef588d3c54b5
Flandre3569 <1332726596@qq.com>
2021-10-26 20:20:08 +0800

    axios封装及解决跨域问题

--- src/main.js
+++ src/main.js
@@ -3,14 +3,20 @@ import App from './App.vue'
 
 // global style
 import './assets/css/index.less';
+// element-plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
 // vue-router
 import router from './router';
 // vuex
 import store from './store';
 
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(ElementPlus);
 
 app.mount('#app');
+
--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,20 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './assets'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: '3000',
+    open: false,
+    proxy: {
+      '/api': {
+        // 在此填写后端接口API
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true, // target是域名的话，需要这个参数，
+        secure: false, // 设置支持https协议的代理
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
